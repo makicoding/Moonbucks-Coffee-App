5499e9abdb8ac2143662b2c3e5625e4b328cdf25
Maki Hirose <Capture@dyn-129-236-212-241.dyn.columbia.edu>
2019-05-22 20:33:48 -0400

    firebase.js updated with code for triggering modals

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -14,100 +14,151 @@ var config = {
   firebase.initializeApp(config);
 
 
-// ABOVE IS FIREBASE CONFIGURATION FOR MOONBUCKS COFFEE APP
-// RECODE FROM BELOW
-// INSERT MODAL INTO INDEX HTML AND STORE HTML
-// NEED FORM VALIDATION
-// DISPLAY TOTAL PRICE TO CUSTOMER ALSO
-
 
 // ----------------------------------------
 // 2. ADD NEW ORDER TO FIREBASE DATABASE
 
 var database = firebase.database();
 
+// Grabs user input
+var newOrderName = $("#name");                             // .val() sets the value of #name that jQuery has gotten ($ is get)     // .trim() trims any white spaces before and after the string, but not in-between the string.
+var newOrderCoffee = $("#item1");
+var newOrderTea = $("#item2");
+var newOrderCroissant = $("#item3");
+var newOrderTime = moment().format('LT');
+
+// Price per item
+var coffeePrice = 2;
+var teaPrice = 3;
+var croissantPrice = 3;
+
+// Convert the quantity of the items from strings to numbers
+var numberNewOrderCoffee = parseInt(newOrderCoffee.val())       // parseInt converts a string into an integer. To convert a string into a decimal number, use parseFloat
+var numberNewOrderTea = parseInt(newOrderTea.val())
+var numberNewOrderCroissant = parseInt(newOrderCroissant.val())
+
+// Calculate total price
+var totalPrice = (numberNewOrderCoffee * coffeePrice) + (numberNewOrderTea * teaPrice) + (numberNewOrderCroissant * croissantPrice);  
+// var totalPrice = newOrderCoffee.val() * coffeePrice;
+
+// use toFixed() to display only 2 decimal places
+var totalPriceTwoDecimalPlaces = totalPrice.toFixed(2);       // the (2) means 2 decimal places.
+
+
+
 // Function occurs when #addNewOrderButton is clicked
 $("#addNewOrderButton").on("click", function(event) {
   event.preventDefault();
 
+  console.log(totalPriceTwoDecimalPlaces);
+
   // --------------------
   // Form validation
-  function validateForm() {
-    var isValid = true;
-    $(".form-control").each(function() {
-        if ($(this).val() === "") {
-            isValid = false;
-        }
-    });
+
+  // function validateForm() {
+  //   var isValid = true;
+
+    // // Get name from .form-control and if the value is empty, return false for isValid
+    // $(".form-control").each(function() {
+    //     if ($(this).val() === "") {
+    //       isValid = false;
+    //     }
+    // });
 
     //$(".chosen-select").each(function() {   
-//
+    //
     //    if ($(this).val() === "") {
     //        isValid = false;
     //    }
     //});
     
-    return isValid;
+  // If the value for #name or (item1 and #item2 and item3) are empty, return false for isValid
+  if ( (!newOrderName.val().trim()) || (!newOrderCoffee.val() && !newOrderTea.val() && !newOrderCroissant.val()) ) {
+  // if (($("#name").val().trim() === "") || (($("#item1").val() === "") && ($("#item2").val() === "") && ($("#item3").val() === ""))) {
+    // isValid = false;
 
+    console.log("Please fill out name and quantity for at least one item before submitting!");
+
+    // Show the modal with alerting the user to fill out all fields
+    $("#pleaseFillAllFieldsModal").modal("toggle");
+
+    return;
   }
-  // --------------------
-  // If all required fields are filled
-  if (validateForm()) {
 
-    // Grabs user input
-    var newOrderName = $("#name").val().trim();                             // .val() sets the value of #name that jQuery has gotten ($ is get)     // .trim() trims any white spaces before and after the string, but not in-between the string.
-    var newOrderCoffee = $("#item1").val();
-    var newOrderTea = $("#item2").val();
-    var newOrderCroissant = $("#item3").val();
-    var newOrderTime = moment().format('LT');
+    // return isValid;
+  // };
+
+  // --------------------
+
+  // If all required fields are filled
+  // if (validateForm()) {
+
+  // Show Modal 2 confirming the user's order before submitting
+  $("#confirmModal").modal("toggle");
+
+  // Populate Modal 2 with order data
+  $("#modalConfirmOrderName").html(newOrderName.val());
+  console.log(totalPriceTwoDecimalPlaces);
+  $("#modalConfirmOrderTotal").html(totalPriceTwoDecimalPlaces);
+
+});
+
+
+// When the confirmButton on Modal 2 is clicked
+$(".confirmButton").click(function () {
 
     // Creates local "temporary" object called newOrder for holding the new order data
     var newOrder = {
-      name: newOrderName,
-      coffee: newOrderCoffee,
-      tea: newOrderTea,
-      croissant: newOrderCroissant,
+      name: newOrderName.val().trim(),
+      coffee: newOrderCoffee.val(),
+      tea: newOrderTea.val(),
+      croissant: newOrderCroissant.val(),
       time: newOrderTime
     };
 
     // Uploads newOrder data to the firebase database
     database.ref().push(newOrder);
-
+    console.log("New order submitted!");
+    orderSuccessModal();
+    
     // Logs everything to console
-    console.log(newOrder.name);
-    console.log(newOrder.coffee);
-    console.log(newOrder.tea);
-    console.log(newOrder.croissant);
-    console.log(newOrder.time);
+    // console.log(newOrder.name);
+    // console.log(newOrder.coffee);
+    // console.log(newOrder.tea);
+    // console.log(newOrder.croissant);
+    // console.log(newOrder.time);
 
-    // Price per item
-    var coffeePrice = 2;
-    var teaPrice = 1.5;
-    var croissantPrice = 3; 
-
-    // Calculate total price
-    var totalPrice = (newOrderCoffee * coffeePrice) + (newOrderTea * teaPrice) + (newOrderCroissant * croissantPrice);
-
-    // use toFixed() to display only 2 decimal places
-    var totalPriceTwoDecimalPlaces = totalPrice.toFixed(2);       // the (2) means 2 decimal places.
-
-    $("#modalMessage").text(`Order submitted successfully! The total price is $${totalPriceTwoDecimalPlaces}`);
-    $("#confirmationModal").modal("toggle");      // this line displays the modal
+    // $("#modalMessage").text(`Order submitted successfully! The total price is $${totalPriceTwoDecimalPlaces}`);
+    // $("#confirmationModal").modal("toggle");      // this line displays the modal
     //alert();    // Code here so that it would confirm to a modal.  Need to include total price.  Also include Cancel and Submit button.
-
-    // Clears all of the text-boxes on the customer order form on index.html
-    $("#name").val("");
-    $("#item1").val("");
-    $("#item2").val("");
-    $("#item3").val("");
   
-  }
+  // }
 
-  else {
-    alert("Please fill out name before submitting!");
-  }
+  // else {
+  //   alert("Please fill out name and quantity for at least one item before submitting!");
+  // }
 
 });
+
+
+// Function to open Modal 3 to let the customer know the order was made successfully
+function orderSuccessModal() {
+  $("#successModal").modal("toggle");
+
+  // Include the customer's name in Modal 3
+  $("#modalSuccessOrderName").html(newOrderName.val().trim());
+
+  $(".successModalCloseButton").click(function () {
+    window.location.href = "/index.html";
+
+    // Clears all of the text-boxes on the customer order form on index.html
+    // newOrderName.val("");
+    // newOrderCoffee.val("");
+    // newOrderTea.val("");
+    // newOrderCroissant.val("");
+  });
+
+};
 
 
 
@@ -156,20 +207,6 @@ database.ref().on("child_added", function(childSnapshot) {
   // Append the new row to the table
   $("#customerOrderTable > tbody").prepend(newRow);
 });
-
-
-
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
-
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
-
-
-
-
 
 
 
